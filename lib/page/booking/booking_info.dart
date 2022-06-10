d8984f51b2f78aad5aa54ea22f23ce7ab392828f
nifhanif <fb.hanif@gmail.com>
2022-06-10 20:30:05 +0700

    redesain button

--- lib/page/booking/booking_info.dart
+++ lib/page/booking/booking_info.dart
@@ -25,112 +25,145 @@ class _BookingInfoState extends State<BookingInfo>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverPersistentHeader(
-            delegate: MySliverAppBar(expandedHeight: 300),
-            pinned: true,
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-                (_, index) => Container(
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(15),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
+      body: Stack(
+        children: [
+          CustomScrollView(
+            slivers: [
+              SliverPersistentHeader(
+                delegate: MySliverAppBar(expandedHeight: 300),
+                pinned: true,
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                    (_, index) => Container(
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(15),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    Text('Lapangan Tenis Senayan').titleText(),
-                                    Text('0,1 Km')
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text('Lapangan Tenis Senayan')
+                                            .titleText(),
+                                        Text('0,1 Km')
+                                      ],
+                                    ),
+                                    Text('Jakarta, Indonesia'),
+                                    const SizedBox(height: 20),
+                                    Text(
+                                        'Kompleks Gelora Bung Karno Unit 4 (Jalan Asia Afrika) Jakarta, DKI Jakarta 10270, Indonesia'),
                                   ],
                                 ),
-                                Text('Jakarta, Indonesia'),
-                                const SizedBox(height: 20),
-                                Text(
-                                    'Kompleks Gelora Bung Karno Unit 4 (Jalan Asia Afrika) Jakarta, DKI Jakarta 10270, Indonesia'),
-                              ],
-                            ),
-                          ),
-                          DefaultTabController(
-                            length: 2,
-                            child: Column(
-                              children: [
-                                TabBar(
-                                  labelColor: Colors.black,
-                                  labelStyle: TextStyle(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w700),
-                                  unselectedLabelStyle: TextStyle(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w400),
-                                  isScrollable: true,
-                                  indicatorColor:
-                                      Theme.of(context).colorScheme.primary,
-                                  onTap: (index) {
-                                    // Tab index when user select it, it start from zero
-                                  },
-                                  tabs: [
-                                    Tab(
-                                      text: "Page Subtitle - Nunito Bold 12",
+                              ),
+                              DefaultTabController(
+                                length: 2,
+                                child: Column(
+                                  children: [
+                                    TabBar(
+                                      labelColor: Colors.black,
+                                      labelStyle: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w700),
+                                      unselectedLabelStyle: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w400),
+                                      isScrollable: true,
+                                      indicatorColor:
+                                          Theme.of(context).colorScheme.primary,
+                                      onTap: (index) {
+                                        // Tab index when user select it, it start from zero
+                                      },
+                                      tabs: [
+                                        Tab(
+                                          text:
+                                              "Page Subtitle - Nunito Bold 12",
 
-                                      // child:
-                                      //     Text("Page Subtitle - Nunito Bold 12")
-                                      //         .fieldTitleText()
-                                      //         .black(),
+                                          // child:
+                                          //     Text("Page Subtitle - Nunito Bold 12")
+                                          //         .fieldTitleText()
+                                          //         .black(),
+                                        ),
+                                        Tab(
+                                          text:
+                                              "Page Subtitle - Nunito Bold 12",
+                                          // child:
+                                          //     Text("Page Subtitle - Nunito Bold 12")
+                                          //         .fieldTitleText()
+                                          //         .black(),
+                                        ),
+                                      ],
                                     ),
-                                    Tab(
-                                      text: "Page Subtitle - Nunito Bold 12",
-                                      // child:
-                                      //     Text("Page Subtitle - Nunito Bold 12")
-                                      //         .fieldTitleText()
-                                      //         .black(),
-                                    ),
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: 400,
-                                  child: TabBarView(
-                                    children: [
-                                      ListView(
+                                    SizedBox(
+                                      height: 400,
+                                      child: TabBarView(
                                         children: [
-                                          Text("Page Subtitle - Nunito Bold 12")
-                                              .fieldTitleText(),
-                                          SizedBox(
-                                            height: 12,
+                                          ListView(
+                                            children: [
+                                              Text("Page Subtitle - Nunito Bold 12")
+                                                  .fieldTitleText(),
+                                              SizedBox(
+                                                height: 12,
+                                              ),
+                                              ItemInfo(),
+                                              ItemInfo(),
+                                              ItemInfo(),
+                                              ItemInfo(),
+                                            ],
                                           ),
-                                          ItemInfo(),
-                                          ItemInfo(),
-                                          ItemInfo(),
-                                          ItemInfo(),
+                                          ListView(
+                                            children: [
+                                              OpeningHour(),
+                                              OpeningHour(),
+                                              OpeningHour(),
+                                              OpeningHour(),
+                                              OpeningHour(),
+                                              OpeningHour(),
+                                              OpeningHour(),
+                                            ],
+                                          )
                                         ],
                                       ),
-                                      ListView(
-                                        children: [
-                                          OpeningHour(),
-                                          OpeningHour(),
-                                          OpeningHour(),
-                                          OpeningHour(),
-                                          OpeningHour(),
-                                          OpeningHour(),
-                                          OpeningHour(),
-                                        ],
-                                      )
-                                    ],
-                                  ),
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
-                        ],
+                        ),
+                    childCount: 1),
+              ),
+            ],
+          ),
+          Positioned(
+              top: 0,
+              child: SafeArea(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Get.back();
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: SvgPicture.asset("assets/ic/ic_back.svg"),
+                        ),
                       ),
-                    ),
-                childCount: 1),
-          )
+                      // Expanded(child: Container()),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: SvgPicture.asset("assets/ic/ic_bookmark.svg"),
+                      ),
+                    ],
+                  ),
+                ),
+              )),
         ],
       ),
       bottomNavigationBar: OButtonBar(
@@ -160,32 +193,6 @@ class MySliverAppBar extends SliverPersistentHeaderDelegate {
           height: 100,
           fit: BoxFit.cover,
         ),
-        Positioned(
-            top: 0,
-            child: SafeArea(
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        Get.back();
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: SvgPicture.asset("assets/ic/ic_back.svg"),
-                      ),
-                    ),
-                    // Expanded(child: Container()),
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: SvgPicture.asset("assets/ic/ic_bookmark.svg"),
-                    ),
-                  ],
-                ),
-              ),
-            )),
         Positioned(
             // top: expandedHeight / 2 - shrinkOffset,
             bottom: 0,
